524e2a26d938578176555fd0381946be6176a26e
Joseph <josephyuanpto@gmail.com>
2020-04-02 15:14:37 -0400

    2nd change

--- Service/app.py
+++ Service/app.py
@@ -6,12 +6,18 @@ def fileReader(comp):
     with open('./'+comp+'.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
     testJson = data
-    stock_price=[]
+    stock_price_close=[]
+    stock_price_high=[]
+    stock_price_low=[]
+    stock_volume=[]
     stock_date=[]
     for i in testJson:
-        stock_price.append(i['Close'])
+        stock_price_close.append(i['Close'])
+        stock_price_high.append(i['High'])
+        stock_price_low.append(i['Low'])
         stock_date.append(i['Time'])
-    return stock_price,stock_date
+        stock_volume.append(i['Volume'])
+    return stock_price_close,stock_price_high,stock_price_low,stock_volume,stock_date
 
 app = Flask(__name__)
 
@@ -23,9 +29,13 @@ text="123"
 def company():
     # print("aaaaa")
     ops = request.args.get('ops')
-    stock_price,stock_date=fileReader(ops)
-    closePrice= stock_price[len(stock_price)-1]
-    result={"res":ops,"close":closePrice}
+    stock_price_close,stock_price_high,stock_price_low,stock_volume,stock_date=fileReader(ops)
+    closePrice= stock_price_close[len(stock_price_close)-1]
+    highPrice= stock_price_high[len(stock_price_high)-1]
+    lowPrice= stock_price_low[len(stock_price_low)-1]
+    volume= stock_volume[len(stock_volume)-1]
+
+    result={"res":ops,"close":closePrice,"High":highPrice,"Low":lowPrice,"Volume":volume,"allDate":stock_date,"allClose":stock_price_close}
     return result
 
 @app.route('/')
--- Service/static/js/Handmade.js
+++ Service/static/js/Handmade.js
@@ -12,11 +12,377 @@ function comp(x) {
             var obj = JSON.parse(responseText);
             var compName = obj['res'];
             var closePrice = obj['close'];
+            var highPrice = obj['High'];
+            var lowPrice = obj['Low'];
+            var volume = obj['Volume'];
+            var alldate=obj['allDate'];
+            var date1=[]
+            for(var i=0;i<alldate.length;i++){
+                date1.push(alldate[i])
+            }
+            var allClose=obj['allClose'];
+            var data1=[]
+            for(var i=0;i<allClose.length;i++){
+                data1.push(allClose[i])
+            }
+
             // document.getElementById("result").innerHTML = data;
             if (true)
-                {document.getElementById("compname").innerHTML = compName;
-                document.getElementById("Close").innerHTML = closePrice;
-                alert("display")}
+                {
+                    document.getElementById("compname").innerHTML = compName;
+                    document.getElementById("Close").innerHTML = closePrice;
+                    document.getElementById("High").innerHTML = highPrice;
+                    document.getElementById("Low").innerHTML = lowPrice;
+                    document.getElementById("Volume").innerHTML = volume;
+
+                    // alert("display")
+                }
+            //  Chart Part
+            var data = {
+                labels: date1,
+                datasets: [{
+                    label: '# of Votes',
+                    data: data1,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                    fill: false
+                }]
+            };
+            var multiLineData = {
+                labels: date1,
+                datasets: [{
+                    label: 'Dataset 1',
+                    data: data1,
+                    borderColor: [
+                        '#587ce4'
+                    ],
+                    borderWidth: 2,
+                    fill: false
+                },
+                    {
+                        label: 'Dataset 2',
+                        data: data1,
+                        borderColor: [
+                            '#ede190'
+                        ],
+                        borderWidth: 2,
+                        fill: false
+                    },
+                    {
+                        label: 'Dataset 3',
+                        data: data1,
+                        borderColor: [
+                            '#f44252'
+                        ],
+                        borderWidth: 2,
+                        fill: false
+                    }
+                ]
+            };
+            var options = {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                legend: {
+                    display: false
+                },
+                elements: {
+                    point: {
+                        radius: 0
+                    }
+                }
+
+            };
+            var doughnutPieData = {
+                datasets: [{
+                    data: data1,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.5)',
+                        'rgba(54, 162, 235, 0.5)',
+                        'rgba(255, 206, 86, 0.5)',
+                        'rgba(75, 192, 192, 0.5)',
+                        'rgba(153, 102, 255, 0.5)',
+                        'rgba(255, 159, 64, 0.5)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                }],
+
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: [
+                    'Pink',
+                    'Blue',
+                    'Yellow',
+                ]
+            };
+            var doughnutPieOptions = {
+                responsive: true,
+                animation: {
+                    animateScale: true,
+                    animateRotate: true
+                }
+            };
+            var areaData = {
+                labels: date1,
+                datasets: [{
+                    label: '# of Votes',
+                    data: data1,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                    fill: true, // 3: no fill
+                }]
+            };
+
+            var areaOptions = {
+                plugins: {
+                    filler: {
+                        propagate: true
+                    }
+                }
+            }
+
+            var multiAreaData = {
+                labels: date1,
+                datasets: [{
+                    label: 'Facebook',
+                    data: data1,
+                    borderColor: ['rgba(255, 99, 132, 0.5)'],
+                    backgroundColor: ['rgba(255, 99, 132, 0.5)'],
+                    borderWidth: 1,
+                    fill: true
+                },
+                    {
+                        label: 'Twitter',
+                        data: data1,
+                        borderColor: ['rgba(54, 162, 235, 0.5)'],
+                        backgroundColor: ['rgba(54, 162, 235, 0.5)'],
+                        borderWidth: 1,
+                        fill: true
+                    },
+                    {
+                        label: 'Linkedin',
+                        data: data1,
+                        borderColor: ['rgba(255, 206, 86, 0.5)'],
+                        backgroundColor: ['rgba(255, 206, 86, 0.5)'],
+                        borderWidth: 1,
+                        fill: true
+                    }
+                ]
+            };
+
+            var multiAreaOptions = {
+                plugins: {
+                    filler: {
+                        propagate: true
+                    }
+                },
+                elements: {
+                    point: {
+                        radius: 0
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            display: false
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            display: false
+                        }
+                    }]
+                }
+            }
+
+            var scatterChartData = {
+                datasets: [{
+                    label: 'First Dataset',
+                    data: [{
+                        x: -10,
+                        y: 0
+                    },
+                        {
+                            x: 0,
+                            y: 3
+                        },
+                        {
+                            x: -25,
+                            y: 5
+                        },
+                        {
+                            x: 40,
+                            y: 5
+                        }
+                    ],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)'
+                    ],
+                    borderWidth: 1
+                },
+                    {
+                        label: 'Second Dataset',
+                        data: [{
+                            x: 10,
+                            y: 5
+                        },
+                            {
+                                x: 20,
+                                y: -30
+                            },
+                            {
+                                x: -25,
+                                y: 15
+                            },
+                            {
+                                x: -10,
+                                y: 5
+                            }
+                        ],
+                        backgroundColor: [
+                            'rgba(54, 162, 235, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgba(54, 162, 235, 1)',
+                        ],
+                        borderWidth: 1
+                    }
+                ]
+            }
+
+            var scatterChartOptions = {
+                scales: {
+                    xAxes: [{
+                        type: 'linear',
+                        position: 'bottom'
+                    }]
+                }
+            }
+            // Get context with jQuery - using jQuery's .get() method.
+            if ($("#barChart").length) {
+                var barChartCanvas = $("#barChart").get(0).getContext("2d");
+                // This will get the first returned node in the jQuery collection.
+                var barChart = new Chart(barChartCanvas, {
+                    type: 'bar',
+                    data: data,
+                    options: options
+                });
+            }
+
+            if ($("#myChart").length) {
+                var lineChartCanvas = $("#myChart").get(0).getContext("2d");
+                var lineChart = new Chart(lineChartCanvas, {
+                    type: 'line',
+                    data: data,
+                    options: options
+                });
+            }
+
+            if ($("#linechart-multi").length) {
+                var multiLineCanvas = $("#linechart-multi").get(0).getContext("2d");
+                var lineChart = new Chart(multiLineCanvas, {
+                    type: 'line',
+                    data: multiLineData,
+                    options: options
+                });
+            }
+
+            if ($("#areachart-multi").length) {
+                var multiAreaCanvas = $("#areachart-multi").get(0).getContext("2d");
+                var multiAreaChart = new Chart(multiAreaCanvas, {
+                    type: 'line',
+                    data: multiAreaData,
+                    options: multiAreaOptions
+                });
+            }
+
+            if ($("#doughnutChart").length) {
+                var doughnutChartCanvas = $("#doughnutChart").get(0).getContext("2d");
+                var doughnutChart = new Chart(doughnutChartCanvas, {
+                    type: 'doughnut',
+                    data: doughnutPieData,
+                    options: doughnutPieOptions
+                });
+            }
+
+            if ($("#pieChart").length) {
+                var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+                var pieChart = new Chart(pieChartCanvas, {
+                    type: 'pie',
+                    data: doughnutPieData,
+                    options: doughnutPieOptions
+                });
+            }
+
+            if ($("#areaChart").length) {
+                var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+                var areaChart = new Chart(areaChartCanvas, {
+                    type: 'line',
+                    data: areaData,
+                    options: areaOptions
+                });
+            }
+
+            if ($("#scatterChart").length) {
+                var scatterChartCanvas = $("#scatterChart").get(0).getContext("2d");
+                var scatterChart = new Chart(scatterChartCanvas, {
+                    type: 'scatter',
+                    data: scatterChartData,
+                    options: scatterChartOptions
+                });
+            }
+
+            if ($("#browserTrafficChart").length) {
+                var doughnutChartCanvas = $("#browserTrafficChart").get(0).getContext("2d");
+                var doughnutChart = new Chart(doughnutChartCanvas, {
+                    type: 'doughnut',
+                    data: browserTrafficData,
+                    options: doughnutPieOptions
+                });
+    }
         } else if (xmlHttp.readyState == 4) {
             var error = "Wrong Input";
             // document.getElementById("result").innerHTML = error;
